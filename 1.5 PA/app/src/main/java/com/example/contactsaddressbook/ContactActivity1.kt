1ad8cf145c924baf8e81504473a413108f867c8e
Wesley Gavitt <wesgav9989@students.ecpi.edu>
2026-08-01 21:21:24 -0400

    Add 1.5 PA - COntacts Address Book

---
+++ 1.5 PA/app/src/main/java/com/example/contactsaddressbook/ContactActivity1.kt
@@ -0,0 +1,16 @@
+// Wesley Gavitt
+// 1.5 PA
+// 8/1/2026
+
+package com.example.contactsaddressbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactActivity1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact1)
+    }
+}
